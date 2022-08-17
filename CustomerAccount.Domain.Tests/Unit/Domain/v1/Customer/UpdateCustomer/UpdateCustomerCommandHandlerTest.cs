ca3e1fe83a2e5464bb724200e1d4e26ed20623fe
fernando.gouvea.lfg@gmail.com <fernando.gouvea.lfg@gmail.com>
2022-08-16 23:28:19 -0300

    Implementa teste UpdateCustomerCommandHandlerTest

---
+++ CustomerAccount.Domain.Tests/Unit/Domain/v1/Customer/UpdateCustomer/UpdateCustomerCommandHandlerTest.cs
@@ -0,0 +1,23 @@
+﻿using AutoFixture;
+using CustomerAccount.Domain.Commands.v1.Customer.UpdateCustomer;
+using CustomerAccount.Domain.Tests.Unit.Domain.v1.Customer.UpdateCustomer.Mocks.Mapper;
+using CustomerAccount.Domain.Tests.Unit.Domain.v1.Customer.UpdateCustomer.Mocks.Repository;
+using NUnit.Framework;
+
+namespace CustomerAccount.Domain.Tests.Unit.Domain.v1.Customer.UpdateCustomer
+{
+    public sealed class UpdateCustomerCommandHandlerTest
+    {
+        [Test]
+        public void UpdateCustomer_ShouldReturnUnit()
+        {
+            var fixture = new Fixture();
+            var mapper = MapperMock.GetMock();
+            var repository = RepositotyMock.MockAddCustomerAsync();
+            var handler = new UpdateCustomerCommandHandler(mapper, repository);
+            var response = handler.Handle(fixture.Create<UpdateCustomerCommandRequest>(), CancellationToken.None);
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
